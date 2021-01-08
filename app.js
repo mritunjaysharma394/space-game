88d6116c6fc69aaee539ecc6749bbf984ff4414a
Mritunjay Sharma <mritunjaysharma394@gmail.com>
2021-01-08 23:51:57 +0530

    add listener for laser

--- app.js
+++ app.js
@@ -182,6 +182,18 @@ function initGame() {
 	eventEmitter.on(Messages.KEY_EVENT_RIGHT, () => {
 		hero.x += 5;
 	});
+
+	eventEmitter.on(Messages.KEY_EVENT_SPACE, () => {
+		if (hero.canFire()) {
+		  hero.fire();
+		}
+	});
+
+	eventEmitter.on(Messages.COLLISION_ENEMY_LASER, (_, { first, second }) => {
+		first.dead = true;
+		second.dead = true;
+	  });
+
 }
 
 function intersectRect(r1, r2) {
